841ad1ee76e9f8e8e0b1b7957a9ff08c468f4f16
Adam <adam2c2000@gmail.com>
2020-12-02 17:45:03 -0800

    removed hardcoded lr

--- votenet/train.py
+++ votenet/train.py
@@ -83,9 +83,6 @@ NUM_POINT = FLAGS.num_point
 MAX_EPOCH = FLAGS.max_epoch
 BASE_LEARNING_RATE = FLAGS.learning_rate
 
-#????
-BASE_LEARNING_RATE = 0.001
-
 BN_DECAY_STEP = FLAGS.bn_decay_step
 BN_DECAY_RATE = FLAGS.bn_decay_rate
 LR_DECAY_STEPS = [int(x) for x in FLAGS.lr_decay_steps.split(',')]
