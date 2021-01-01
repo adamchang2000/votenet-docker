fd862b189da2c7640874d40f2aacc50fae251884
Adam Chang <adamchang2000@berkeley.edu>
2021-01-01 15:19:40 -0800

    cls_nms false

--- votenet/train.py
+++ votenet/train.py
@@ -248,7 +248,7 @@ TEST_VISUALIZER = TfVisualizer(FLAGS, 'test')
 
 # Used for AP calculation
 CONFIG_DICT = {'remove_empty_box':False, 'use_3d_nms':True,
-    'nms_iou':0.25, 'use_old_type_nms':False, 'cls_nms':True,
+    'nms_iou':0.25, 'use_old_type_nms':False, 'cls_nms':False,
     'per_class_proposal': True, 'conf_thresh':0.05,
     'dataset_config':DATASET_CONFIG}
 
