587c405f7e70ec7d28e22c4d593f0f45c6217e7b
Adam Chang <adamchang2000@berkeley.edu>
2021-02-04 14:19:44 -0800

    multiply box loss by 1000

--- votenet/models/loss_helper.py
+++ votenet/models/loss_helper.py
@@ -341,7 +341,7 @@ def get_loss(end_points, config):
     #LOSS MULTIPLIERS
     VOTE_LOSS_MULTIPLIER = 1000 #votes are not even close to the object, different parts of the scene are voting for the object lol
     OBJECTNESS_LOSS_MULTIPLIER = 0.5
-    BOX_LOSS_MULTIPLIER = 1
+    BOX_LOSS_MULTIPLIER = 1000
     ROTATION_LOSS_MULTIPLIER = 0.1
 
     # Vote loss
